4b144e47e11ba58f4cafab531b12ae19d7e3ba64
Hemanta Pradhan <hemantapxy@gmail.com>
2024-09-09 23:44:21 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function toggleContent(sectionId) {
+    var content = document.getElementById(sectionId);
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
